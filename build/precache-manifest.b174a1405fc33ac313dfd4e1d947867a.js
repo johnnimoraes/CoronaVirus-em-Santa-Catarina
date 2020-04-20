a2a606215764c9acd72517c7fb48f1c2a4000133
JohnniMoraes <johnnimoraes@gmail.com>
2020-04-20 20:38:58 -0300

    Configurando app para deploy

---
+++ build/precache-manifest.b174a1405fc33ac313dfd4e1d947867a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb270efea4089ec3b4dcc6fbc51f035",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad5b2e80958439fffdd",
+    "url": "/static/js/2.d2f417ad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d2f417ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b1520a3fbee70ed490d",
+    "url": "/static/js/main.bb71e809.chunk.js"
+  },
+  {
+    "revision": "4b19a1a4d34058288dce",
+    "url": "/static/js/runtime-main.6e5bbb43.js"
+  }
+]);
